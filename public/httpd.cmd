acd810b850bfb36b6894dc3ac143fe023ca7b3e6
yapcheahshen@gmail.com <yapcheahshen@gmail.com>
2021-10-06 11:27:52 +0630

    add miniweb

--- public/httpd.cmd
+++ public/httpd.cmd
@@ -1,3 +1,4 @@
 @rem https://github.com/avih/miniweb/releases/tag/snapshot-2017-09-17
+@rem (miniweb options)  miniweb.exe -h
 @start http://127.0.0.1:8080/
-@miniweb.exe -p 8080 -i 127.0.0.1 -r public -d -l NUL
+@cmd/C miniweb.bin -p 8080 -i 127.0.0.1 -r . -d -l NUL
